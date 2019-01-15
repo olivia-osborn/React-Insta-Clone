8dfc69370e23e72aa23b3fdb7d7f51555bae089a
Olivia Osborn <olioz95@gmail.com>
2019-01-14 22:53:03 -0500

    styled search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./SearchBar.css";
 
 function SearchBar(props) {
     return(
-        <div>
-            <img src="" alt="insta-logo"/>
-            <input type="text" placeHolder="search"/>
+        <div className="search-bar">
+            <div>
+                <img className="logo" src={require("./instagram-logo.png")} alt="insta-logo"/>
+                <img className="title" src={require("./instagram-logo-text-black-png.png")} alt="insta-title" />
+            </div>
+            <input className="search" type="text" placeHolder="Search"/>
+            <div>
+                <a className="icon" href="null"><img src={require("./navigation.png")} alt="nav-icon"/></a>
+                <a className="icon" href="null"><img src={require("./heart.png")} alt="heart-icon"/></a>
+                <a className="icon" href="null"><img src={require("./user.png")} alt="user-icon"/></a>
+            </div>
         </div>
     )
 }

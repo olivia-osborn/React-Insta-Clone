ceafbe2fd4a7480a8228552119a4cf3166189ede
Olivia Osborn <olioz95@gmail.com>
2019-01-15 16:00:47 -0500

    made commentSection component a class

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { Component } from 'react';
 import Comment from "./Comment/Comment";
 import "./CommentSection.css";
 
-function CommentSection(props) {
-    return(
-        <div>
-            <div className="comments">
-                {props.listOfComments.map((comment, index) => {
-                    return <Comment comment={comment} key={index}/>;
-                })}
+class CommentSection extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            comments: props.listOfComments
+        }
+    }
+
+    
+    render() {
+        return(
+            <div>
+                <div className="comments">
+                    {this.state.comments.map((comment, index) => {
+                        return <Comment comment={comment} key={index}/>;
+                    })}
+                </div>
+                <input className="add-comment" type="text" placeholder="Add a comment..."/>
             </div>
-            <input className="add-comment" type="text" placeholder="Add a comment..."/>
-        </div>
-    )
+        )
+    }
 }
 
 export default CommentSection;
